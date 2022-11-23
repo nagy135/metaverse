a1cf993cbddb12b131fb8f1c23a9b31be68e3472
Viktor Nagy <viktor.nagy@01people.com>
2022-11-23 15:03:37 +0100

    feat(app): localStorage token

--- app/src/components/login-switcher.tsx
+++ app/src/components/login-switcher.tsx
@@ -6,7 +6,18 @@ interface IProps {
   children: JSX.Element;
 }
 
+const JWT_TOKEN_KEY = 'jwt-token';
+
 export default ({ children }: IProps) => {
-  const { jwtToken } = useContext(JwtTokenContext);
+  let { jwtToken, setJwtToken } = useContext(JwtTokenContext);
+
+  // NOTE: try getting it from localStorage if present
+  if (jwtToken){
+    localStorage.setItem(JWT_TOKEN_KEY, jwtToken);
+  } else {
+    const localJwtToken = localStorage.getItem(JWT_TOKEN_KEY);
+    if (localJwtToken) setJwtToken(localJwtToken);
+  }
+
   return jwtToken ? children : <Login />;
 };
