3233eaaacada225b573a9f9d4312e6f14a5a59d0
Viktor Nagy <viktor.nagy@01people.com>
2022-11-23 13:33:24 +0100

    fix(api): cors allow prod origin

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://metaverse.infiniter.eu'
+    ],
     methods: ['POST', 'PUT', 'DELETE', 'GET', 'OPTIONS', 'HEAD'],
   });
   await app.listen(3000);
