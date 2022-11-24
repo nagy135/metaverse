04252abff2a98bdda960c2a6a52e9630177f0485
Viktor Nagy <viktor.nagy@01people.com>
2022-11-24 09:45:10 +0100

    feat(app): logout button

--- app/src/components/login-switcher.tsx
+++ app/src/components/login-switcher.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { JwtTokenContext } from "../App";
 import Login from "./login";
 
@@ -6,18 +6,36 @@ interface IProps {
   children: JSX.Element;
 }
 
-const JWT_TOKEN_KEY = 'jwt-token';
+const JWT_TOKEN_KEY = "jwt-token";
 
 export default ({ children }: IProps) => {
   let { jwtToken, setJwtToken } = useContext(JwtTokenContext);
 
   // NOTE: try getting it from localStorage if present
-  if (jwtToken){
+  if (jwtToken) {
     localStorage.setItem(JWT_TOKEN_KEY, jwtToken);
   } else {
     const localJwtToken = localStorage.getItem(JWT_TOKEN_KEY);
     if (localJwtToken) setJwtToken(localJwtToken);
   }
 
-  return jwtToken ? children : <Login />;
+  const handleLogOut = useCallback(() => {
+    setJwtToken(null);
+    localStorage.removeItem(JWT_TOKEN_KEY);
+  }, []);
+
+  const content = jwtToken ? (
+    <div>
+      <div className="flex justify-end">
+        <button onClick={() => handleLogOut()} className="btn btn-error m-2">
+          Log out
+        </button>
+      </div>
+      {children}
+    </div>
+  ) : (
+    children
+  );
+
+  return jwtToken ? content : <Login />;
 };
