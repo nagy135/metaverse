aa36979334dd81de42e48d99de2a7946b6de1012
Viktor Nagy <viktor.nagy@365.bank>
2023-11-11 16:08:36 +0100

    feat(api): cors infiniter.tech

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://metaverse.infiniter.eu'
-    ],
+    origin: ['http://localhost:3000', 'https://metaverse.infiniter.tech'],
     methods: ['POST', 'PUT', 'DELETE', 'GET', 'OPTIONS', 'HEAD'],
   });
   await app.listen(3000);
